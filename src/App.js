60c204555d289987e29649aa4f14d32db93dfe6b
Mandi Wise <hello@mandiwise.com>
2019-12-19 10:53:27 -0700

    Animate the intro with Greensock.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useEffect, useRef } from "react";
 
 import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
+  const intro = useRef();
+
+  useEffect(() => {
+    gsap.to(intro.current, { opacity: 1, delay: 1, duration: 4.5 });
+    gsap.to(intro.current, { opacity: 0, delay: 5.5, duration: 1.5 });
+  }, []);
+
   return (
-    <div>
-      <p>Hello, GreenSock World!</p>
+    <div className="container">
+      <section className="intro" ref={intro}>
+        <p>
+          A long time ago, in a galaxy far,
+          <br /> far away....
+        </p>
+      </section>
     </div>
   );
 }
