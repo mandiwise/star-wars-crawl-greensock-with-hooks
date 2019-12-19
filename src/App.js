a8c1cd6c701ea55c4e964342ce8fe9ff2c87cf8e
Mandi Wise <hello@mandiwise.com>
2019-12-19 15:43:40 -0700

    Animate text scrolling.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import logo from "./logo.svg";
 function App() {
   const intro = useRef();
   const title = useRef();
+  const content = useRef();
 
   useEffect(() => {
     let tl = new gsap.timeline();
@@ -19,7 +20,8 @@ function App() {
       .to(intro.current, { opacity: 0, duration: 1.5 })
       .set(title.current, { opacity: 1, scale: 2.75 })
       .to(title.current, { scale: 0.05, ease: "power2", duration: 8 })
-      .to(title.current, { opacity: 0, duration: 1.5 }, "-=1.5");
+      .to(title.current, { opacity: 0, duration: 1.5 }, "-=1.5")
+      .to(content.current, { top: "-170%", duration: 200 });
   }, []);
 
   return (
@@ -32,6 +34,27 @@ function App() {
       </section>
       <section className="title" ref={title}>
         <img src={logo} alt="Code Wars title" />
+      </section>
+      <section className="crawl">
+        <div className="content" ref={content}>
+          <h1 className="episode-number">Episode 7</h1>
+          <h2 className="episode-title">THE APP AWAKENS</h2>
+          <p>
+            The Development Team Lead has vanished. In her absence, the sinister
+            FUNCTIONAL BUG has risen from the ashes of the CI Tool and will not
+            rest until the last developer has been destroyed.
+          </p>
+          <p>
+            With the support of the QA TEAM, the Software Developer leads a
+            brave RESISTANCE. He is desperate to find his Lead and gain her help
+            in restoring peace and justice to the repository.
+          </p>
+          <p>
+            The Developer has sent his most daring editor theme on a secret
+            mission to the production branch, where an old ally has discovered a
+            clue to the Lead’s whereabouts....
+          </p>
+        </div>
       </section>
     </div>
   );
