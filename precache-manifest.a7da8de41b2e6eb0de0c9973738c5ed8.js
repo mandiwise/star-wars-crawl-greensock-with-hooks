67750746d55f1b928812b0842f239075991ad79e
Mandi Wise <hello@mandiwise.com>
2019-12-20 12:10:51 -0700

    Updates

---
+++ precache-manifest.a7da8de41b2e6eb0de0c9973738c5ed8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1608d82c12b8bd66611551dabc76d4",
+    "url": "/star-wars-crawl-greensock-with-hooks/index.html"
+  },
+  {
+    "revision": "b5ccfa41f9fc4e527d63",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/css/main.289ccf88.chunk.css"
+  },
+  {
+    "revision": "4cee3a2a38c912780f70",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/js/2.6754caaf.chunk.js"
+  },
+  {
+    "revision": "04f37191b74afac41f68b5e481713297",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/js/2.6754caaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5ccfa41f9fc4e527d63",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/js/main.46434b61.chunk.js"
+  },
+  {
+    "revision": "ac84de7dfbc2fd2dc3d8",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/js/runtime-main.cbd25eb0.js"
+  },
+  {
+    "revision": "d17c00c9418f6acb7283bd6fb6ebc778",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/media/logo.d17c00c9.svg"
+  },
+  {
+    "revision": "a3d484ffcde19609f9c6030366f74157",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/media/volume_off.a3d484ff.svg"
+  },
+  {
+    "revision": "ef7f87ab516ccf1eac5a5111815c427f",
+    "url": "/star-wars-crawl-greensock-with-hooks/static/media/volume_on.ef7f87ab.svg"
+  }
+]);
